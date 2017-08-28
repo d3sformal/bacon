25d293cdcf8b7229635f60c56dfa10eb417d93eb
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2017-08-28 12:05:58 +0200

    use simpler notation

--- test/CountTo5.hs
+++ test/CountTo5.hs
@@ -28,9 +28,9 @@ inv :: Show (e 'BooleanSort) => Either (Cex e) (Inv e) -> IO ()
 inv (Left  (Cex cs)) = error    . ("failed with counterexample: " ++) . show $ cs
 inv (Right (Inv iv)) = putStrLn . ("succeeded with invariant: "   ++) . show $ iv
 
-main = mapM_ (\(sink, p) -> sink =<< runSolver defaultLog ( ic3 vs init t p )) [ (inv, not (pc .=. cnst 2) .|. not (n .<. i))
-                                                                               , (cex, not (pc .=. cnst 2) .|. i .<. n)
-                                                                               , (cex, not (pc .=. cnst 2) .|. not (cnst 0 .<. i))
-                                                                               , (inv, not (pc .=. cnst 2) .|. not (cnst 5 .<. i))
-                                                                               , (cex, not (pc .=. cnst 2) .|. i .<. cnst 5)
-                                                                               , (inv, not (pc .=. cnst 2) .|. not (cnst 10 .<. i)) ]
+main = mapM_ (\(sink, p) -> sink =<< runSolver defaultLog ( ic3 vs init t p )) [ (inv, pc .=. cnst 2 .->. not (n .<. i))
+                                                                               , (cex, pc .=. cnst 2 .->. i .<. n)
+                                                                               , (cex, pc .=. cnst 2 .->. not (cnst 0 .<. i))
+                                                                               , (inv, pc .=. cnst 2 .->. not (cnst 5 .<. i))
+                                                                               , (cex, pc .=. cnst 2 .->. i .<. cnst 5)
+                                                                               , (inv, pc .=. cnst 2 .->. not (cnst 10 .<. i)) ]
